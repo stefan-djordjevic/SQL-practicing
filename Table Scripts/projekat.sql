ce7b6422dd632d63e00157875453e092bdb4d660
Stefan Djordjevic <56848402+stefan-djordjevic@users.noreply.github.com>
2020-02-09 17:23:25 +0100

    Add files via upload

---
+++ Table Scripts/projekat.sql
@@ -0,0 +1,8 @@
+insert into projekat values (10,100, 'Optimizacija u spicu','Elektrosrbija');
+insert into projekat values (20, 70, 'Implementacija SCADA sistema','ED Beograd');
+insert into projekat values (30, 50, 'DMS Implementation','Hydro One Networks');
+insert into projekat values (40, 50, 'Load Types Analysis','Enel');
+insert into projekat values (50, 70, 'OMS Implementation','Austin Energy');
+insert into projekat values (60,100, 'Instalacija indikatora kvara','Elektrovojvodina');
+insert into projekat values (70,100, 'Network Data Gap Analysis','MOESK');
+insert into projekat values (80,100, 'Data Warehouse','SA Power Networks');
